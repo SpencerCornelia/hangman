09eda17011d2b240e026f54273194828c21e8129
S <s>
2017-03-15 23:18:56 -0700

    {"types":"C","runs":42}

--- Hangman.java
+++ Hangman.java
@@ -103,8 +103,7 @@ public class Hangman extends ConsoleProgram {
 	private String createWord(String theWord) {
 		// need to run for loop for length of theWord
 		for (int i = 0; i <= theWord.length(); i++) {
-			char c = guessedLetters[i];
-			println(c);
+			
 		}
 		return currentWord;
 	} 
