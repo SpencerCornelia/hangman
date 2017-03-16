4efd1cf3c99ac9cbb91bdf6edd8a387fdc63b762
S <s>
2017-03-15 22:17:51 -0700

    {"types":"C","runs":17}

--- Hangman.java
+++ Hangman.java
@@ -100,7 +100,6 @@ public class Hangman extends ConsoleProgram {
 	private void createWord(String word) {
 		for (int i = 0; i < word.length(); i++) {
 			currentWord += "- ";
-			guessedLetters[i] = (String)"- ";
 		}
 	} 
 	
