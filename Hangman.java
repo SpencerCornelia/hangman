c88a488b809b56ab34b97301a3faddb04246f54b
S <s>
2017-03-15 22:48:52 -0700

    {"types":"C","runs":23}

--- Hangman.java
+++ Hangman.java
@@ -102,7 +102,7 @@ public class Hangman extends ConsoleProgram {
 		for (int i = 0; i < theWord.length(); i++) {
 			if (true) {
 				//Character.isLetter(guessedLetters[i])
-				
+				currentWord = "spencer";
 			} else {
 				currentWord += "- ";
 			}
