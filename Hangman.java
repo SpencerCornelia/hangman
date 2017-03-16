7d138ee042576c6ed6c879d318f17abd821acf5d
S <s>
2017-03-15 20:48:57 -0700

    {"types":"C","runs":6}

--- Hangman.java
+++ Hangman.java
@@ -72,6 +72,7 @@ public class Hangman extends ConsoleProgram {
 		println("Welcome to Hangman");
 		while (guessesRemaining > 0) {
 			println("Your word now looks like this: " + currentWord);
+			println("You now have " + guessesRemaining " guesses left");
 		}
 	}
 	
