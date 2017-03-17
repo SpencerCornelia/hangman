69875bcaf27a05be891a0a7c4d6ebdaa98e16655
S <s>
2017-03-16 20:12:00 -0700

    {"types":"C","runs":47}

--- Hangman.java
+++ Hangman.java
@@ -108,7 +108,11 @@ public class Hangman extends ConsoleProgram {
 	} 
 	
 	private char getLetterFromUser() {
-		readLine("Please enter a letter: ");
+		/*
+		 * try this when I get here
+		 * String letter = readLine("Please enter a letter: ")
+		 */
+		System.out.println("Please enter a letter: ");
 		letter = input.next().charAt(0);
 		if (!Character.isLetter(letter)) {
 			println("Please enter only a letter");
