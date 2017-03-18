36a138399a7d38f77c39ecb60c29a45589ba3ed2
S <s>
2017-03-17 19:55:38 -0700

    {"types":"C","runs":119}

--- Hangman.java
+++ Hangman.java
@@ -92,7 +92,7 @@ public class Hangman extends ConsoleProgram {
 	private void addKarel() {
 		GImage karel = new GImage("karel.png");
 		karel.setSize(100, 100);
-		canvas.add(karel, canvas.getWidth() / 2, canvas.getHeight() - 200);
+		canvas.add(karel, 150, canvas.getHeight() - 200);
 	}
 	
 	private void playGame(String word) {
