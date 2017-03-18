70c5336e69506c65a886ad7b19d388c94a4ed8ba
S <s>
2017-03-17 19:46:01 -0700

    {"types":"C","runs":107}

--- Hangman.java
+++ Hangman.java
@@ -74,7 +74,6 @@ public class Hangman extends ConsoleProgram {
 	 ***********************************************************/
 	
 	public void run() {
-		//theWord = "ABCDEF";
 		theWord = getRandomWord();
 		currentWord = createWord(theWord);
 		playGame(theWord);
